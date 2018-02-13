74fb14fc0b43d9bd2ba1eeaf3cc9e98a2cb5883f
Abhijit Theophilus <abhijit.theo@gmail.com>
2018-02-13 15:39:00 +0530

    Changed result to OptimizeResult.

--- pso/minimize.py
+++ pso/minimize.py
@@ -1,65 +1,100 @@
+import functools as ft
 import numpy as np
-from scipy.spatial import distance
+from .minimize import _conmin_pso
+from .utils import _gen_confunc
 
 
-def _pso(fun, x0, confunc=None, friction=.8, g_rate=.1, l_rate=.1,
-         max_velocity=1., max_iter=1e5, stable_iter=1e4, ptol=1e-5,
-         ctol=1e-5, callback=None):
-    """Internal function to minimize the objective function through PSO.
-
-    See Also
-    --------
-    For the rest of the documentation, see ``pso.minimize_pso``.
+def minimize_pso(fun, x0, args=(), constraints=(), tol=None, callback=None,
+                 options=None):
+    """Minimize the scalar function through particle swarm optimization.
 
     Parameters
     ----------
-    confunc : callable
-        The function constructed by check_constraints. Should return the
-        constraint matrix.
+    fun : callable
+        The objective function to be minimized. Must be in the form
+        ``f(x, *args)``. The optimizing argument, ``x``, is a 1-D array of
+        points, and ``args`` is a tuple of any additional fixed parameters
+        needed to completely specify the function.
+    x0 : array_like of shape (N, D)
+        Initial position to begin PSO from, where ``N`` is the number of points
+        and ``D`` the dimensionality of each point.
+    args : tuple, optional
+        Extra arguments passed to the objective function.
+    constraints : tuple, optional
+        Constraints definition. Each constraint is defined in a dictionary with
+        fields:
+
+            type : str
+                Constraint type: ‘eq’ for equality, ‘ineq’ for inequality.
+            fun : callable
+                The function defining the constraint.
+            args : sequence, optional
+                Extra arguments to be passed to the function.
+
+        Equality constraint means that the constraint function result is to be
+        zero whereas inequality means that it is to be non-negative.
+    tol : float, optional
+        Tolerance for termination and constraint adjustment. For detailed
+        control, use options.
+    callback : callable, optional
+        Called after each iteration on each solution vector.
+    options : dict, optional
+        A dictionary of solver options:
+
+            friction: float, optional
+                Velocity is scaled by friction before updating, default 0.8.
+                    ``velocity = friction*velocity + dv_g + dv_l``
+            g_rate: float, optional
+                Global learning rate, default 0.1.
+                    ``dv_g = g_rate * random(0, 1) * (gbest - current)``
+            l_rate: float, optional
+                Local learning rate, default 0.1.
+                    ``dv_l = l_rate * random(0, 1) * (lbest - current)``
+            max_velocity: float, optional
+                Threshold for velocity, default 1.0.
+            max_iter: int, optional
+                Maximum iterations to wait for convergence, default 10000.
+            stable_iter: int, optional
+                Number of iterations to wait before Swarm is declared stable,
+                default 1000.
+            ptol: float, optional
+                Change in position should be greater than ``ptol`` to update,
+                otherwise particle is considered stable, default 1e-5.
+            ctol: float, optional
+                Acceptable error in constraint satisfaction, default 1e-5.
+            sttol : float, optional
+                Tolerance to convert strict inequalities to non-strict
+                inequalities, default 1e-6.
+            eqtol : float, optional
+                Tolerance to convert equalities to non-strict inequalities,
+                default 1e-7.
+
+    Returns
+    -------
+    res : scipy.optimize.OptimizeResult
+        The optimization result represented as a OptimizeResult object.
+
+    ==> need to add notes, references, examples
+        --> Document extra parameters in OptimizeResult
+        --> Explain method used for convergence.
     """
-    # Initial position and velocity.
-    position = x0
-    velocity = np.random.uniform(-max_velocity, max_velocity, position.shape)
+    x0 = np.asarray(x0)
+    if x0.dtype.kind in np.typecodes["AllInteger"]:
+        x0 = np.asarray(x0, dtype=float)
 
-    # Initial local best for each point and global best.
-    lbest = position.copy()
-    gbest = lbest[np.argmax(fun(lbest))]
+    if tol is not None:
+        options.setdefault('ptol', tol)
+        options.setdefault('ctol', tol)
 
-    stable_count = 0
-    for ii in range(max_iter):
-        # Store old for threshold comparison.
-        gbest_fit = fun(gbest)
+        sttol = options.pop('sttol', tol)
+        eqtol = options.pop('eqtol', tol)
 
-        # Determine the velocity gradients.
-        leaders = np.argmin(
-            distance.cdist(position, lbest, 'sqeuclidean'), axis=1)
-        dv_g = g_rate * np.random.uniform(0, 1) * (gbest - position)
-        dv_l = l_rate * np.random.uniform(0, 1) * (lbest[leaders] - position)
+    fun_ = ft.update_wrapper(lambda x: fun(x, *args), fun)
 
-        # Update velocity such that |velocity| <= max_velocity.
-        velocity *= friction
-        velocity += (dv_g + dv_l)
-        chk = (np.abs(velocity) > max_velocity)
-        velocity[chk] = np.sign(velocity[chk]) * max_velocity
+    if constraints:
+        confunc = _gen_confunc(constraints, sttol, eqtol)
+        result = _conmin_pso(fun_, x0, confunc, **options)
+    else:
+        result = None
 
-        # Update the local and global bests.
-        position += velocity
-        to_update = (np.apply_along_axis(fun, 1, position) < fun(lbest))
-        if confunc:
-            to_update &= (confunc(position).sum(axis=1) < ctol)
-
-        if to_update.any():
-            lbest[to_update] = position[to_update]
-            gbest = lbest[np.argmax(fun(lbest))]
-
-        # Termination criteria.
-        if np.abs(gbest_fit - fun(gbest)) < ptol:
-            stable_count += 1
-            if stable_count == stable_iter:
-                return (gbest, ii)
-        else:
-            stable_count = 0
-
-        # Final callback.
-        if callback is not None:
-            position = np.apply_along_axis(callback, 1, position)
+    return result
