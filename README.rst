38c0bb93b79fbd9b0bf1d8a3eaa4dcc0d129b903
Abhijit Theophilus <abhijit.theo@gmail.com>
2018-02-15 20:34:59 +0530

    Moved files to src and edited setup.py

--- README.rst
+++ README.rst
@@ -1,3 +1,7 @@
-===========================
-Particle Swarm Optimization
-===========================
+============================================================
+PSOPy: Python implementation for Particle Swarm Optimization
+============================================================
+
+PSOPy (pronounced "Soapy") is a SciPy compatible super fast Python implementation for Particle Swarm Optimization.
+
+.. Need to add more stuff here.
--- setup.py
+++ setup.py
@@ -1,42 +1,58 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
+"""PSOPy: Python implementation for Particle Swarm Optimization
+
+.. Add a description here
 """
 
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
+from __future__ import absolute_import
+from __future__ import print_function
 
-here = path.abspath(path.dirname(__file__))
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
 
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    return open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf-8')
+    ).read()
+
 
 setup(
     name='psopy',
     version='0.0.1a1',
     description='A SciPy compatible super fast Python implementation for '
                 'Particle Swarm Optimization. ',
-    long_description=long_description,
-    # url='https://github.com/pypa/sampleproject',
+    long_description='%s\n%s' % (
+        re.compile('^.. start-badges.*^.. end-badges',
+                   re.M | re.S).sub('', read('README.rst')),
+        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    ),
     author='Abhijit Theophilus, Dr. Snehanshu Saha, Suryoday Basak',
-    author_email='abhijit.theo@gmail.com, snehanshusaha@gmail.com, ' \
+    author_email='abhijit.theo@gmail.com, snehanshusaha@gmail.com, '
                  'suryodaybasak@gmail.com',
+    url='https://github.com/jerrytheo/psopy',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
 
     # Classifiers list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Mathematics',
-
-        # License used.
         'License :: OSI Approved :: BSD License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
 
         # To be done later ...(Need to add support for Python 2.)
         # Specify the Python versions you support here. In particular, ensure
@@ -48,23 +64,19 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-
-    keywords='particle-swarm-optimization constrained-optimization',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
-
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['scipy', 'numpy'],
-    extras_require={
-        'test': ['pytest'],         # $ pip install psopy[test]
-    },
-
-    # To be done later ...
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.
-    package_data={
-        'psopy': [],
-    },
-
-    # Look up entry_points if scripting required.
-
+    keywords=[
+        'optimization-algorithms',
+        'particle-swarm-optimization',
+        'constrained-optimization',
+    ],
+    install_requires=[
+        'scipy',
+        'numpy'
+    ],
+    setup_requires=[
+        'pytest-runner'
+    ],
+    tests_requires=[
+        'pytest'
+    ],
 )
